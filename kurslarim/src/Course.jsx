0a87557e2ea62d4bb5c1b8d043ca33013b907dba
Ali Kemal Çalak <kemalcalak@gmail.com>
2024-07-24 10:43:24 +0300

    Transition between courses enabled

--- kurslarim/src/Course.jsx
+++ kurslarim/src/Course.jsx
@@ -1,17 +1,16 @@
 function Course({ id, content, title, price, removeOneCourse }) {
-    return (
-      <div className="card">
-        <div className="cardTitlePrice">
-          <h2 className="cardTitle">{title}</h2>
-          <h4 className="cardPrice">{price}TL</h4>
-        </div>
-        <p>{content}</p>
-        <button className="cardDeleteBtn" onClick={() => removeOneCourse(id)}>
-          Sil
-        </button>
+  return (
+    <div className="card">
+      <div className="cardTitlePrice">
+        <h2 className="cardTitle">{title}</h2>
+        <h4 className="cardPrice">{price}TL</h4>
       </div>
-    );
-  }
-  
-  export default Course;
-  
+      <p>{content}</p>
+      <button className="cardDeleteBtn" onClick={() => removeOneCourse(id)}>
+        Sil
+      </button>
+    </div>
+  );
+}
+
+export default Course;
--- kurslarim/src/Courses.jsx
+++ kurslarim/src/Courses.jsx
@@ -1,23 +1,69 @@
 import Course from './Course';
+import { useState } from 'react';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 function Courses({ courses, removeCourse }) {
-  //   console.log(courses);
+  const [index, setIndex] = useState(0);
+  const { content, title, price } = courses[index];
+
+  console.log(index);
+
+  const checkIndex = (index) => {
+    debugger;
+    if (index < 0) {
+      return courses.length - 1;
+    }
+    if (index > courses.length - 1) {
+      return 0;
+    }
+    return index;
+  };
+
+  const getRandomCourse = () => {
+    let randomNumber = Math.floor(Math.random() * courses.length);
+
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+
+    setIndex(checkIndex(randomNumber));
+  };
+
+  const prevCourse = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkIndex(newIndex);
+    });
+  };
+  const nextCourse = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkIndex(newIndex);
+    });
+  };
+
   return (
     <div className="courseMainDiv">
-      <div>
+      <div className="courseTitleAndButton">
         <h2>Kurslarım</h2>
+        <button className="cardDeleteBtn" onClick={getRandomCourse}>
+          Rastgele Kurs Ata!
+        </button>
       </div>
       <div className="cardDiv">
-        {courses.map((course) => {
-          return (
-            <Course
-              key={course.id}
-              {...course}
-              removeOneCourse={removeCourse}
-            />
-          );
-          //   course={course}
-        })}
+        <button className="prevNextBtn" onClick={prevCourse}>
+          <FaChevronLeft />
+        </button>
+        <div className="card">
+          <div className="cardTitlePrice">
+            <h2 className="cardTitle">{title}</h2>
+            <h4 className="cardPrice">{price}TL</h4>
+          </div>
+          <p>{content}</p>
+        </div>
+        <button className="prevNextBtn" onClick={nextCourse}>
+          <FaChevronRight />
+        </button>
       </div>
     </div>
   );
