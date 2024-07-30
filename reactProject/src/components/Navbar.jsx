d91acd3d4cb73d23a77b7e6b2ecdcc1f46e05e5d
Ali Kemal Çalak <kemalcalak@gmail.com>
2024-07-30 15:37:39 +0300

    feat: Implement responsive layout and enhance UI components with Material UI 5
    - Added responsive design features to improve mobile usability.
    - Integrated various Material UI components including Toolbar, Avatar, and Checkbox.
    - Customized theme settings for consistent styling across the application.
    - Implemented a modal with Tooltip and Fab for enhanced user interaction.
    - Improved overall layout using Flexbox and Grid systems.

---
+++ reactProject/src/components/Navbar.jsx
@@ -0,0 +1,108 @@
+import { Mail, Notifications, Pets } from "@mui/icons-material";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Menu,
+  MenuItem,
+  styled,
+  Toolbar,
+  Typography,
+  Switch,
+} from "@mui/material";
+import React, { useState } from "react";
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#333" : "white", // Conditional background color
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  display: "none",
+  alignItems: "center",
+  gap: "20px",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
+}));
+
+const Navbar = ({ mode, setMode }) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          Kemal Calak
+        </Typography>
+        <Pets sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase
+            placeholder="search..."
+            sx={{
+              color: mode === "dark" ? "white" : "black", // Text color based on mode
+              "&::placeholder": {
+                color: mode === "dark" ? "lightgray" : "gray", // Placeholder color based on mode
+              },
+            }}
+          />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={2} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src=""
+            onClick={() => setOpen(true)}
+          />
+        </Icons>
+        <UserBox onClick={() => setOpen(true)}>
+          <Avatar sx={{ width: 30, height: 30 }} src="" />
+          <Typography variant="span">Kemal Çalak</Typography>
+        </UserBox>
+      </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={() => setOpen(false)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
+    </AppBar>
+  );
+};
+
+export default Navbar;
